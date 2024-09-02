813e2e592b83d5f032f6082e3bc32e6c605ce8ee
SHAIK IMTIAZ ALI <69511017+shaikimtiazali@users.noreply.github.com>
2024-09-02 22:50:37 +0530

    add Header Component and made changes in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
+    <Header>
       <h1>Hello World Hosted From Jenkins docker Image</h1>
       <p>pipeline written by shaik imtiaz ali</p>
       <p>Working on auto triggering jenkins pipeline</p>
-    </div>
+    </Header>
   );
 }
 
