f0e30bc23ba109af4ce1f13d115133e412d6dd9a
SHAIK IMTIAZ ALI <69511017+shaikimtiazali@users.noreply.github.com>
2024-09-02 21:39:26 +0530

    add jenkins docker image text in app.js file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World Hosted From Aws Amplify</h1>
+      <h1>Hello World Hosted From Jenkins docker Image</h1>
     </div>
   );
 }
