0cccd147ecb1b111811ca5cb7fe149f28d882079
SHAIK IMTIAZ ALI <69511017+shaikimtiazali@users.noreply.github.com>
2024-09-02 22:06:19 +0530

    added another p tag

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>Hello World Hosted From Jenkins docker Image</h1>
       <p>pipeline written by shaik imtiaz ali</p>
+      <p>Working on auto triggering jenkins pipeline</p>
     </div>
   );
 }
