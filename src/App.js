0c8263a4517ba1c9ec06759b2453a42c3ad0f5a8
SHAIK IMTIAZ ALI <69511017+shaikimtiazali@users.noreply.github.com>
2024-09-03 12:17:17 +0530

    added hello world

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <Header>
-      <h1>Hello World Hosted From Jenkins docker Image</h1>
+      <h1>Hello World</h1>
       <p>pipeline written by shaik imtiaz ali</p>
       <p>Working on auto triggering jenkins pipeline</p>
       <p>Added Docker push to docker hub with auto tag generation</p>
