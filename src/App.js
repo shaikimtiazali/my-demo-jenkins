4e13c257d8e95191bf057003b2521347d2e1cfb1
SHAIK IMTIAZ ALI <69511017+shaikimtiazali@users.noreply.github.com>
2024-09-05 22:28:33 +0530

    Update App.js
    add new h1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <Header>
       <h1>Hello Aapmor</h1>
+      <h1>Hello Imtiaz</h1>
       <p>pipeline written by shaik imtiaz ali</p>
       <p>Working on auto triggering jenkins pipeline</p>
       <p>Added Docker push to docker hub with auto tag generation</p>
